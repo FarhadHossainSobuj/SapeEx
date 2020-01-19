ef185a4d70f4e9103b23b5b2b5f86601f08b364a
tonysaha <tonyjpi69@gmail.com>
2020-01-19 12:27:36 +0600

    Add Date Picker

--- app/src/main/java/com/tushar/sapeex/AppDatabase.java
+++ app/src/main/java/com/tushar/sapeex/AppDatabase.java
@@ -2,12 +2,12 @@ package com.tushar.sapeex;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-import androidx.room.migration.Migration;
-import androidx.sqlite.db.SupportSQLiteDatabase;
+
 
 @Database(entities = {UserModel.class,ExpencessModel.class},version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract UserDao dao();
+
 
 }
--- app/src/main/java/com/tushar/sapeex/ExpencessActivity.java
+++ app/src/main/java/com/tushar/sapeex/ExpencessActivity.java
@@ -3,17 +3,20 @@ package com.tushar.sapeex;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class ExpencessActivity extends AppCompatActivity {
 
-    private  EditText ET_type,ET_ammount;
+    private  EditText ET_type,ET_ammount,ET_date;
     private Button BT_savee;
     private List<ExpencessModel> list;
     @Override
@@ -22,6 +25,7 @@ public class ExpencessActivity extends AppCompatActivity {
         setContentView(R.layout.activity_expencess);
         ET_type=findViewById(R.id.ET_type);
         ET_ammount=findViewById(R.id.ET_ammount);
+        ET_date=findViewById(R.id.ET_Date);
 
         BT_savee=findViewById(R.id.BT_savee);
 
@@ -50,5 +54,26 @@ public class ExpencessActivity extends AppCompatActivity {
 
             }
         });
+
+        ET_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dateChose();
+            }
+        });
+    }
+
+
+    //Method for Date Picker......................................................
+    void dateChose(){
+        Calendar c=Calendar.getInstance();
+        DatePickerDialog datePick=new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+            }
+        },c.get(Calendar.YEAR),c.get(Calendar.MONTH),c.get(Calendar.DAY_OF_MONTH));
+
+        datePick.show();
     }
 }
