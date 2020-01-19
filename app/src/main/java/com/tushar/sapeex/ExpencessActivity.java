5e8b6430e33853ee500c3401827a2cfbf24c88c2
tonysaha <tonyjpi69@gmail.com>
2020-01-19 12:51:56 +0600

    Add Date Picker update

--- app/src/main/java/com/tushar/sapeex/ExpencessActivity.java
+++ app/src/main/java/com/tushar/sapeex/ExpencessActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.List;
@@ -67,9 +68,14 @@ public class ExpencessActivity extends AppCompatActivity {
     //Method for Date Picker......................................................
     void dateChose(){
         Calendar c=Calendar.getInstance();
+
         DatePickerDialog datePick=new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+              //  Toast.makeText(getApplicationContext(), String.valueOf(year),Toast.LENGTH_SHORT);
+
+                String date=String.valueOf(dayOfMonth)+"-"+String.valueOf(month+1)+"-"+String.valueOf(year);
+                ET_date.setText(date);
 
             }
         },c.get(Calendar.YEAR),c.get(Calendar.MONTH),c.get(Calendar.DAY_OF_MONTH));
